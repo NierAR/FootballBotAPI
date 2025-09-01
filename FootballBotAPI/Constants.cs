742a503ab6b0c2be7f8c5cdc76feef80c01b0a35
NierAR <154439907+NierAR@users.noreply.github.com>
2025-09-01 15:33:40 +0300

    Update Constants.cs

--- FootballBotAPI/Constants.cs
+++ FootballBotAPI/Constants.cs
@@ -2,9 +2,9 @@
 {
     internal class Constants
     {
-        public static readonly string ApiKey = "19b6038f65155ca1c59df88f83aac531";
-        public static readonly string Host = "v3.football.api-sports.io";
-        public static readonly string Token = "6657591558:AAEbn5jAjE_mt6nrgFXChx81tI21SlthBBc";
-        public static readonly string Connection = "Host=localhost;Port=5432;Database=postgres;User Id=postgres;Password=1827;";
+        public static readonly string ApiKey = "";
+        public static readonly string Host = "";
+        public static readonly string Token = "";
+        public static readonly string Connection = "";
     }
 }
